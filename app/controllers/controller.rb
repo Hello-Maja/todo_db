2ee5f0ffa672ebdbceb72e9be60ddb1976cc2873
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-02 14:27:28 -0500

    did some controller, require ActiveRecord

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -1,6 +1,36 @@
-
+require_relative '../models/task.rb'
 
 class Controller 
+  attr_accessor :control_flow, :task
 
+  def initialize
+    run! if validated
+    Viewer.help
+  end
 
+  def validated
+    unless ARGV.empty?
+      self.control_flow = ARGV.shift
+      self.task = ARGV.join unless ARGV.empty?
+      return true 
+    end
+    false
+  end
+
+  def run!
+    
+    case control_flow
+    
+    when "add"
+    
+    when "list"
+
+    when "remove"
+
+    when "complete"
+
+    else
+
+    end
+  end
 end
